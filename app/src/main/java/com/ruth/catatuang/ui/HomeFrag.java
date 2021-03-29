25dfa660d13286c256310ab08bc3eb514b0e9a9c
Septian Puji <septianpujipuji@gmail.com>
2021-03-29 20:35:05 +0700

    First Push

---
+++ app/src/main/java/com/ruth/catatuang/ui/HomeFrag.java
@@ -0,0 +1,32 @@
+package com.ruth.catatuang.ui;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.ruth.catatuang.R;
+
+public class HomeFrag extends Fragment {
+
+    @SuppressLint("SetTextI18n")
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+        textView.setText("This is home fragment");
+
+        return root;
+    }
+}
